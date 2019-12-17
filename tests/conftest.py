da7d66a9319db2bd81328eca0268b35c36f20d70
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-12-17 17:01:55 +0100

    Add API JSON validation tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,1 @@
-import pytest
-
-from reforis_storage import blueprint
-
-
 pytest_plugins = 'reforis.test_utils.fixtures'
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from flask import current_app
 
@@ -37,3 +39,38 @@ def _check_called_foris_controller_module(sender_mock, module, action):
     actions = [call[1][1] for call in sender_mock.send.mock_calls]
     assert module in modules
     assert action in actions
+
+
+@pytest.mark.parametrize(
+    'data, message', [
+        ({}, "Invalid JSON"),
+        ({'drives': '', 'raid': ''}, {'drives': 'Expected data of type: list'}),
+        ({'drives': [], 'raid': []}, {'raid': 'Expected data of type: str'}),
+    ]
+)
+def test_prepare_srv_post_invalid_data(client, data, message):
+    response = client.post('/storage/api/prepare-srv', json=data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json == message
+
+
+def test_prepare_srv_post_valid_data(client):
+    response = client.post('/storage/api/prepare-srv', json={'drives': [], 'raid': ''})
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'data, message', [
+        ({}, "Invalid JSON"),
+        ({'uuid': []}, {'uuid': 'Expected data of type: str'}),
+    ]
+)
+def test_update_srv_post_invalid_data(client, data, message):
+    response = client.post('/storage/api/update-srv', json=data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json == message
+
+
+def test_update_srv_post_valid_data(client):
+    response = client.post('/storage/api/update-srv', json={'uuid': ""})
+    assert response.status_code == HTTPStatus.OK
