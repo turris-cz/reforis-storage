bb887fe6a956b1b5d5b42ac323fa6e00ce1107ef
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-04-26 17:51:51 +0200

    Bump v0.2.2
    * Add & update translations
    * Improve descriptions of actions in the UUID table
    * Improve headings and help texts
    * Hide DrivesOperations during operations on a drive
    * Fix pendingMigrationAlert behavior
    * NPM audit fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class StorageBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='0.2.1',
+    version='0.2.2',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
