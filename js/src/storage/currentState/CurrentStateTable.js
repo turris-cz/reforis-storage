ebd012d851ae6ad61d16c91f4313994e3a65c332
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-10-07 16:59:41 +0200

    Migrate to FontAwesome v6

--- js/src/storage/currentState/CurrentStateTable.js
+++ js/src/storage/currentState/CurrentStateTable.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -7,6 +7,8 @@
 
 import React from "react";
 
+import { faHdd } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SpinnerElement } from "foris";
 import PropTypes from "prop-types";
 
@@ -28,57 +30,53 @@ export default function CurrentStateTable({
     storageIsPending,
 }) {
     return (
-        <div className="mb-3">
-            <div className="row no-gutters justify-content-center">
-                <div className="col-md-6 d-flex flex-column align-items-center justify-content-center">
-                    <i className="fas fa-hdd fa-4x mb-1" />
-                    <h5>{current_device}</h5>
-                </div>
-                <div className="col-md-6">
-                    <div className="card-body">
-                        <div className="table-responsive">
-                            <table className="table table-borderless table-hover">
-                                <tbody>
-                                    {!Object.keys(
-                                        NOT_PENDING_STORAGE_STATES
-                                    ).includes(state) && (
-                                        <tr>
-                                            <th scope="row">{_("State")}</th>
-                                            <td>
-                                                <div className="d-flex flex-row">
-                                                    {storageIsPending ? (
-                                                        <SpinnerElement small>
-                                                            &nbsp;
-                                                            {
-                                                                STORAGE_STATES[
-                                                                    state
-                                                                ]
-                                                            }
-                                                        </SpinnerElement>
-                                                    ) : (
-                                                        STORAGE_STATES[state]
-                                                    )}
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    )}
-                                    {current_device !== "none" && (
-                                        <tr>
-                                            <th scope="row">{_("Device")}</th>
-                                            <td>{current_device}</td>
-                                        </tr>
-                                    )}
+        <div className="row no-gutters justify-content-center">
+            <div className="col-md-6 d-flex flex-column align-items-center justify-content-center">
+                <FontAwesomeIcon icon={faHdd} size="4x" className="mb-2" />
+                <h5>{current_device}</h5>
+            </div>
+            <div className="col-md-6">
+                <div className="card-body">
+                    <div className="table-responsive">
+                        <table className="table table-borderless table-hover">
+                            <tbody>
+                                {!Object.keys(
+                                    NOT_PENDING_STORAGE_STATES
+                                ).includes(state) && (
                                     <tr>
-                                        <th scope="row">{_("UUID")}</th>
-                                        <td>{uuid === "" ? _("N/A") : uuid}</td>
+                                        <th scope="row">{_("State")}</th>
+                                        <td>
+                                            <div className="d-flex flex-row">
+                                                {storageIsPending ? (
+                                                    <SpinnerElement
+                                                        small
+                                                        className="text-primary"
+                                                    >
+                                                        {STORAGE_STATES[state]}
+                                                    </SpinnerElement>
+                                                ) : (
+                                                    STORAGE_STATES[state]
+                                                )}
+                                            </div>
+                                        </td>
                                     </tr>
+                                )}
+                                {current_device !== "none" && (
                                     <tr>
-                                        <th scope="row">{_("RAID")}</th>
-                                        <td>{raid}</td>
+                                        <th scope="row">{_("Device")}</th>
+                                        <td>{current_device}</td>
                                     </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                )}
+                                <tr>
+                                    <th scope="row">{_("UUID")}</th>
+                                    <td>{uuid === "" ? _("N/A") : uuid}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">{_("RAID")}</th>
+                                    <td>{raid}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
